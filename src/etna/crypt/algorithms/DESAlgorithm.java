aaaf559a1816aa94bf12ee18b5eb62b3c02f1218
Itadakimas <said.ahemt@gmail.com>
2015-10-15 02:06:39 +0200

    creates etna.crypt.algorithms package

---
+++ src/etna/crypt/algorithms/DESAlgorithm.java
@@ -0,0 +1,9 @@
+package etna.crypt.algorithms;
+
+public class DESAlgorithm
+{
+  public DESAlgorithm()
+  {
+    System.out.println("des instanciation");
+  }
+}
--- src/main.java
+++ src/main.java
@@ -1,7 +1,10 @@
+import etna.crypt.algorithms.*;
+
 class Main
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello World! My name is Said AHEMT"); // Display the string.
+    System.out.println("Hello World! My name is Said AHEMT"); // Display the string
+    DESAlgorithm des = new DESAlgorithm();
   }
 }
