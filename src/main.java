c0728f878a6359b0d176001de91d36623145cced
Itadakimas <said.ahemt@gmail.com>
2015-11-07 18:03:20 +0100

    uses static call to KeySchedule

--- src/main.java
+++ src/main.java
@@ -5,7 +5,6 @@ class Main
     public static void main(String[] args)
     {
         System.out.println("Hello World! My name is Said AHEMT"); // Display the string
-        DESAlgorithm des = new DESAlgorithm();
-        des.KeySchedule("totoland", 15);
+        DESAlgorithm.KeySchedule("hello world", 15);
     }
 }
