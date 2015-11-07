443f41db50fa0fc06726edd21ae355fffd6775dc
Itadakimas <said.ahemt@gmail.com>
2015-11-07 17:58:23 +0100

    fixes indentation

--- src/main.java
+++ src/main.java
@@ -2,10 +2,10 @@ import etna.crypt.algorithms.*;
 
 class Main
 {
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World! My name is Said AHEMT"); // Display the string
-    DESAlgorithm des = new DESAlgorithm();
-    des.KeySchedule("totoland", 15);
-  }
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! My name is Said AHEMT"); // Display the string
+        DESAlgorithm des = new DESAlgorithm();
+        des.KeySchedule("totoland", 15);
+    }
 }
