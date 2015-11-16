e067d4f7c1101b4296d6d8d2baf5024589a0729a
Itadakimas <said.ahemt@gmail.com>
2015-11-16 23:18:42 +0100

    implements processE method

--- src/etna/crypt/algorithms/DESAlgorithm.java
+++ src/etna/crypt/algorithms/DESAlgorithm.java
@@ -175,6 +175,42 @@ public class DESAlgorithm
         }
         return ArrayUtils.toPrimitive(bytesList.toArray(new Byte[numberOfBytes]));
     }
+    private static byte[]       processE(byte[] bytes)
+    {
+        int             i;
+        int             inputLength;
+        int             j;
+        int             tableLength;
+        int             tableRowLength;
+        int[]           tableRow;
+        int[][]         table = {
+            {32, 1,  2,  3,  4,  5},
+            {4,  5,  6,  7,  8,  9},
+            {8,  9,  10, 11, 12, 13},
+            {12, 13, 14, 15, 16, 17},
+            {16, 17, 18, 19, 20, 21},
+            {20, 21, 22, 23, 24, 25},
+            {24, 25, 26, 27, 28, 29},
+            {28, 29, 30, 31, 32, 1}
+        };
+        String          input;
+        StringBuilder   output;
+
+        input = binaryToString(bytes);
+        inputLength = input.length();
+        output = new StringBuilder();
+        tableLength = table.length;
+        for (i = 0; i < tableLength; i++)
+        {
+            tableRow = table[i];
+            tableRowLength = tableRow.length;
+            for (j = 0; j < tableRowLength; j++)
+            {
+                output.append(input.charAt(inputLength - tableRow[j]));
+            }
+        }
+        return stringToBinary(output.toString());
+    }
     private static byte[]       processFP(byte[] bytes)
     {
         int             i;
