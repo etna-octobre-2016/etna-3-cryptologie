913827c66b8fc5d526c978a7f991bfc3d039c201
Itadakimas <said.ahemt@gmail.com>
2015-11-17 08:49:49 +0100

    optimizes code

--- src/etna/crypt/algorithms/DESAlgorithm.java
+++ src/etna/crypt/algorithms/DESAlgorithm.java
@@ -175,7 +175,7 @@ public class DESAlgorithm
         }
         return ArrayUtils.toPrimitive(bytesList.toArray(new Byte[numberOfBytes]));
     }
-    private static byte[]       processE(byte[] bytes) throws DESAlgorithmException
+    private static byte[]       permutate(byte[] bytes, int[][] table)
     {
         int             i;
         int             inputLength;
@@ -183,7 +183,27 @@ public class DESAlgorithm
         int             tableLength;
         int             tableRowLength;
         int[]           tableRow;
-        int[][]         table = {
+        String          input;
+        StringBuilder   output;
+
+        input = binaryToString(bytes);
+        inputLength = input.length();
+        output = new StringBuilder();
+        tableLength = table.length;
+        for (i = 0; i < tableLength; i++)
+        {
+            tableRow = table[i];
+            tableRowLength = tableRow.length;
+            for (j = 0; j < tableRowLength; j++)
+            {
+                output.append(input.charAt(inputLength - tableRow[j]));
+            }
+        }
+        return stringToBinary(output.toString());
+    }
+    private static byte[]       processE(byte[] bytes) throws DESAlgorithmException
+    {
+        int[][] table = {
             {32, 1,  2,  3,  4,  5},
             {4,  5,  6,  7,  8,  9},
             {8,  9,  10, 11, 12, 13},
@@ -193,37 +213,16 @@ public class DESAlgorithm
             {24, 25, 26, 27, 28, 29},
             {28, 29, 30, 31, 32, 1}
         };
-        String          input;
-        StringBuilder   output;
 
         if (bytes.length != 4)
         {
             throw new DESAlgorithmException("Invalid E input. Must be 32 bits long. Number of bits provided: " + (bytes.length * 8));
         }
-        input = binaryToString(bytes);
-        inputLength = input.length();
-        output = new StringBuilder();
-        tableLength = table.length;
-        for (i = 0; i < tableLength; i++)
-        {
-            tableRow = table[i];
-            tableRowLength = tableRow.length;
-            for (j = 0; j < tableRowLength; j++)
-            {
-                output.append(input.charAt(inputLength - tableRow[j]));
-            }
-        }
-        return stringToBinary(output.toString());
+        return permutate(bytes, table);
     }
     private static byte[]       processFP(byte[] bytes) throws DESAlgorithmException
     {
-        int             i;
-        int             inputLength;
-        int             j;
-        int             tableLength;
-        int             tableRowLength;
-        int[]           tableRow;
-        int[][]         table = {
+        int[][] table = {
             {40, 8, 48, 16, 56, 24, 64, 32},
             {39, 7, 47, 15, 55, 23, 63, 31},
             {38, 6, 46, 14, 54, 22, 62, 30},
@@ -233,37 +232,16 @@ public class DESAlgorithm
             {34, 2, 42, 10, 50, 18, 58, 26},
             {33, 1, 41,  9, 49, 17, 57, 25}
         };
-        String          input;
-        StringBuilder   output;
 
         if (bytes.length != 8)
         {
             throw new DESAlgorithmException("Invalid FP input. Must be 64 bits long. Number of bits provided: " + (bytes.length * 8));
         }
-        input = binaryToString(bytes);
-        inputLength = input.length();
-        output = new StringBuilder();
-        tableLength = table.length;
-        for (i = 0; i < tableLength; i++)
-        {
-            tableRow = table[i];
-            tableRowLength = tableRow.length;
-            for (j = 0; j < tableRowLength; j++)
-            {
-                output.append(input.charAt(inputLength - tableRow[j]));
-            }
-        }
-        return stringToBinary(output.toString());
+        return permutate(bytes, table);
     }
     private static byte[]       processIP(byte[] bytes) throws DESAlgorithmException
     {
-        int             i;
-        int             inputLength;
-        int             j;
-        int             tableLength;
-        int             tableRowLength;
-        int[]           tableRow;
-        int[][]         table = {
+        int[][] table = {
             {58, 50, 42, 34, 26, 18, 10, 2},
             {60, 52, 44, 36, 28, 20, 12, 4},
             {62, 54, 46, 38, 30, 22, 14, 6},
@@ -273,63 +251,27 @@ public class DESAlgorithm
             {61, 53, 45, 37, 29, 21, 13, 5},
             {63, 55, 47, 39, 31, 23, 15, 7}
         };
-        String          input;
-        StringBuilder   output;
 
         if (bytes.length != 8)
         {
             throw new DESAlgorithmException("Invalid IP input. Must be 64 bits long. Number of bits provided: " + (bytes.length * 8));
         }
-        input = binaryToString(bytes);
-        inputLength = input.length();
-        output = new StringBuilder();
-        tableLength = table.length;
-        for (i = 0; i < tableLength; i++)
-        {
-            tableRow = table[i];
-            tableRowLength = tableRow.length;
-            for (j = 0; j < tableRowLength; j++)
-            {
-                output.append(input.charAt(inputLength - tableRow[j]));
-            }
-        }
-        return stringToBinary(output.toString());
+        return permutate(bytes, table);
     }
     private static byte[]       processP(byte[] bytes) throws DESAlgorithmException
     {
-        int             i;
-        int             inputLength;
-        int             j;
-        int             tableLength;
-        int             tableRowLength;
-        int[]           tableRow;
-        int[][]         table = {
+        int[][] table = {
             {16, 7,  20, 21, 29, 12, 28, 17},
             {1,  15, 23, 26, 5,  18, 31, 10},
             {2,  8,  24, 14, 32, 27, 3,  9},
             {19, 13, 30, 6,  22, 11, 4,  25}
         };
-        String          input;
-        StringBuilder   output;
 
         if (bytes.length != 4)
         {
             throw new DESAlgorithmException("Invalid P input. Must be 32 bits long. Number of bits provided: " + (bytes.length * 8));
         }
-        input = binaryToString(bytes);
-        inputLength = input.length();
-        output = new StringBuilder();
-        tableLength = table.length;
-        for (i = 0; i < tableLength; i++)
-        {
-            tableRow = table[i];
-            tableRowLength = tableRow.length;
-            for (j = 0; j < tableRowLength; j++)
-            {
-                output.append(input.charAt(inputLength - tableRow[j]));
-            }
-        }
-        return stringToBinary(output.toString());
+        return permutate(bytes, table);
     }
     private static byte[]       processPC1(byte[] bytes) throws DESAlgorithmException
     {
@@ -395,13 +337,7 @@ public class DESAlgorithm
     }
     private static byte[]       processPC2(byte[] bytes) throws DESAlgorithmException
     {
-        int             i;
-        int             inputLength;
-        int             j;
-        int             tableLength;
-        int             tableRowLength;
-        int[]           tableRow;
-        int[][]         table = {
+        int[][] table = {
             {14, 17, 11, 24, 1,  5,  3,  28},
             {15, 6,  21, 10, 23, 19, 12, 4},
             {26, 8,  16, 7,  27, 20, 13, 2},
@@ -409,27 +345,12 @@ public class DESAlgorithm
             {51, 45, 33, 48, 44, 49, 39, 56},
             {34, 53, 46, 42, 50, 36, 29, 32}
         };
-        String          input;
-        StringBuilder   output;
 
         if (bytes.length != 7)
         {
             throw new DESAlgorithmException("Invalid PC2 input. Must be 56 bits long. Number of bits provided: " + (bytes.length * 8));
         }
-        input = binaryToString(bytes);
-        inputLength = input.length();
-        output = new StringBuilder();
-        tableLength = table.length;
-        for (i = 0; i < tableLength; i++)
-        {
-            tableRow = table[i];
-            tableRowLength = tableRow.length;
-            for (j = 0; j < tableRowLength; j++)
-            {
-                output.append(input.charAt(inputLength - tableRow[j]));
-            }
-        }
-        return stringToBinary(output.toString());
+        return permutate(bytes, table);
     }
     private static byte[]       shiftBinary(byte[] bytes, int count, boolean isCircular)
     {
