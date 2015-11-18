efcfc5a10639ce3778b03c67128fa014285bfb2d
Itadakimas <said.ahemt@gmail.com>
2015-11-18 23:53:13 +0100

    implements processS8 method

--- src/etna/crypt/algorithms/DESAlgorithm.java
+++ src/etna/crypt/algorithms/DESAlgorithm.java
@@ -424,6 +424,16 @@ public class DESAlgorithm
         };
         return substitute(binaryString, table);
     }
+    private static String       processS8(String binaryString) throws DESAlgorithmException
+    {
+        int[][] table = {
+            {13, 2,  8,  4,  6,  15, 11, 1,  10, 9,  3,  14, 5,  0,  12, 7},
+            {1,  15, 13, 8,  10, 3,  7,  4,  12, 5,  6,  11, 0,  14, 9,  2},
+            {7,  11, 4,  1,  9,  12, 14, 2,  0,  6,  10, 13, 15, 3,  5,  8},
+            {2,  1,  14, 7,  4,  10, 8,  13, 15, 12, 9,  0,  3,  5,  6,  11}
+        };
+        return substitute(binaryString, table);
+    }
     private static byte[]       shiftBinary(byte[] bytes, int count, boolean isCircular)
     {
         long        number;
