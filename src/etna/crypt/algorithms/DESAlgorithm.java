ca25f148db7c6212d5426fb4fe52234641f22704
Itadakimas <said.ahemt@gmail.com>
2015-11-18 23:48:54 +0100

    implements processS1 method

--- src/etna/crypt/algorithms/DESAlgorithm.java
+++ src/etna/crypt/algorithms/DESAlgorithm.java
@@ -354,6 +354,16 @@ public class DESAlgorithm
         }
         return permutate(bytes, table);
     }
+    private static String       processS1(String binaryString) throws DESAlgorithmException
+    {
+        int[][] table = {
+            {14, 4,  13, 1,  2,  15, 11, 8,  3,  10, 6,  12, 5,  9,  0,  7},
+            {0,  15, 7,  4,  14, 2,  13, 1,  10, 6,  12, 11, 9,  5,  3,  8},
+            {4,  1,  14, 8,  13, 6,  2,  11, 15, 12, 9,  7,  3,  10, 5,  0},
+            {15, 12, 8,  2,  4,  9,  1,  7,  5,  11, 3,  14, 10, 0,  6,  13}
+        };
+        return substitute(binaryString, table);
+    }
     private static String       processS2(String binaryString) throws DESAlgorithmException
     {
         int[][] table = {
